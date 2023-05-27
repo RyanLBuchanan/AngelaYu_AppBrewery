934288256a75e1bd230995319a4904bc91b3d403
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-27 12:18:05 -0600

    feat: Completed Banker Roulette exercise

--- Assignments/Day_4/banker_roulette/main.py
+++ Assignments/Day_4/banker_roulette/main.py
@@ -1,19 +1,24 @@
-# Import the random module here
 import random
 
-# Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
+# Get the names input from the user
+names_string = input("Give me everybody's names, separated by a comma: ")
+
+# Split the names string into a list of names
 names = names_string.split(", ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-
+# Generate a random index to select a person to pay
 num_to_pay = random.randint(0, len(names))
 
-print(num_to_pay)
+# Print length of names list
+print(f"Length of names list: {len(names)}")
 
+# Print the random number
+print(f"Random number generated: {num_to_pay}")
+
+# Get the person to pay based on the random index
 person_to_pay = names[num_to_pay]
 
+# Print the selected person who will buy the meal
 print(f"{person_to_pay} is going to buy the meal today!")
 
-# List for people at table: Ryan, Chapel, Alita, Six, Seven
+# List of people at the table: Ryan, Chapel, Alita, Six, Seven
