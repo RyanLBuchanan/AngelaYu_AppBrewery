78f96b0af96145df6a28cd2c386b8783d5c2e518
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-28 10:40:23 -0600

    feat: Completed Student Scores exercise

--- Assignments/Day_5/student_scores/main.py
+++ Assignments/Day_5/student_scores/main.py
@@ -1,17 +1,19 @@
 # 🚨 Don't change the code below 👇
 student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
+    student_scores[n] = int(student_scores[n])
 print(student_scores)
 # 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
+# Write your code below this row 👇
+# Initialize the highest_score variable with the first score in the list
+highest_score = student_scores[0]
 
+# Iterate over each score in the list starting from the second score
 for score in student_scores:
-    highest_score = student_scores[score]
+    # Compare the current score to the highest_score
     if score > highest_score:
         highest_score = score
-        
 
-
+# Print the highest score
 print(f"The highest score in the class is: {highest_score}")
