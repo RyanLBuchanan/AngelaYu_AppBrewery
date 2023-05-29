9ba4cbbc3df0b5152ab8770f296085606e93f076
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-29 09:48:13 -0600

    feat: Started functions exercise

---
+++ Assignments/Day_6/functions/main.py
@@ -0,0 +1,11 @@
+# print("Hello I love you won't you tell me your name.")
+#
+# num_char = len("Hello I love you won't you tell me your name.")
+#
+# print(num_char)
+
+def my_function():
+    print("\nHello I love you won't you tell me your name.")
+    print("\nWell, my odds of success dramatically improve with each attempt.")
+
+my_function()
