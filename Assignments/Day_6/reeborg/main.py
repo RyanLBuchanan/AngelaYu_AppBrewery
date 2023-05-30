2826e68d9289d3f3b19ff846bd83928bdd299a14
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-29 19:50:33 -0600

    feat: Completed Reeborg Hurdle 4 exercise

--- Assignments/Day_6/reeborg/main.py
+++ Assignments/Day_6/reeborg/main.py
@@ -1,35 +1,36 @@
 def turn_around():
+    # Function to turn around by calling turn_left twice
     turn_left()
     turn_left()
-
 
 def turn_right():
+    # Function to turn right by calling turn_left three times
     for _ in range(3):
         turn_left()
 
-
 def make_square_counterclockwise():
+    # Function to make a square counterclockwise
     for _ in range(4):
         move()
         turn_left()
 
-
 def make_square_clockwise():
+    # Function to make a square clockwise
     for _ in range(4):
         move()
         turn_right()
 
-
 def hurdle():
-    #     move()
+    # Function to jump over a hurdle
     turn_left()
-    move()
+    while wall_on_right():
+        move()
     turn_right()
     move()
     turn_right()
-    move()
+    while front_is_clear():
+        move()
     turn_left()
-
 
 # Call make_square_counterclockwise function
 # make_square_counterclockwise()
@@ -41,6 +42,7 @@ def hurdle():
 # for _ in range(6):
 #     hurdle()
 
+# Hurdle 1
 # num_hurdles = 6
 
 # while num_hurdles > 0:
@@ -48,9 +50,18 @@ def hurdle():
 #     num_hurdles -= 1
 #     print(num_hurdles)
 
+# Hurdle 2
 # while not at_goal():
 #     hurdle()
 
+# Hurdle 3
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         hurdle()
+
+# Hurdle 4
 while not at_goal():
     if front_is_clear():
         move()
