c776635923a1359cc65883f7789d86df8565cd39
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-29 18:33:50 -0600

    feat: Completed Reeborg Hurdle exercise

--- Assignments/Day_6/reeborg/main.py
+++ Assignments/Day_6/reeborg/main.py
@@ -2,39 +2,37 @@ def turn_around():
     turn_left()
     turn_left()
 
-
-def turn_right_around():
-    turn_around()
-    turn_left()
-
-
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-
 def make_square_counterclockwise():
-    move()
-    turn_left()
-    move()
-    turn_left()
-    move()
-    turn_left()
-    move()
-
+    for _ in range(4):
+        move()
+        turn_left()
 
 def make_square_clockwise():
+    for _ in range(4):
+        move()
+        turn_right()
+
+def hurdle():
+    move()
     turn_left()
     move()
     turn_right()
     move()
     turn_right()
     move()
-    turn_right()
-    move()
+    turn_left()
 
+# Call make_square_counterclockwise function
+make_square_counterclockwise()
 
-# make_square_clockwise()
-# make_square_counterclockwise()
+# Call make_square_clockwise function
+make_square_clockwise()
 
+# Call hurdle function 6 times
+for _ in range(6):
+    hurdle()
