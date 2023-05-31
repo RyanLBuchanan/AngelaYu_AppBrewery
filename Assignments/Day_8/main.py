e85554d7ff0b352c4fd878ed880cfefe51c6a284
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-05-31 13:21:04 -0600

    feat: Started Day 8

---
+++ Assignments/Day_8/main.py
@@ -0,0 +1,22 @@
+def my_function():
+    # Do this
+    # Then do this
+    # Finally,  do this
+    print("Do this")
+
+# The parameter is the name af the data being passed in, the variable
+def greet(name):
+    print(f"Greetings Earthlings, and you {name}!")
+    print(f"Please do not be alarmed... This means you {name}.")
+    print("I am your new Overlord.")
+
+# Pass in the argument "Ryan"
+# greet("Ryan")
+
+def greet_with(name, location):
+    print(f"Greetings {name}.")
+    print(f"What is it like here on {location}?")
+
+greet_with("Ryan", "Earth")
+
+
