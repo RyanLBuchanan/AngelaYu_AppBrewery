2868e10c965b4c1372e5948097395efe0a281b67
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-01 21:58:47 -0600

    feat: Completed Travel Log exercise

--- Assignments/Day_9/travel_log/main.py
+++ Assignments/Day_9/travel_log/main.py
@@ -1,24 +1,32 @@
+# Create an initial list of dictionaries representing travel log entries
 travel_log = [
-{
-  "country": "France",
-  "visits": 12,
-  "cities": ["Paris", "Lille", "Dijon"]
-},
-{
-  "country": "Germany",
-  "visits": 5,
-  "cities": ["Berlin", "Hamburg", "Stuttgart"]
-},
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
 ]
-#🚨 Do NOT change the code above
 
-# Write the function that will allow new countries
-#to be added to the travel_log. 👇
+# Function to add a new country entry to the travel log
+def add_new_country(country, visits, cities):
+    # Create a new dictionary for the new country
+    new_country_dictionary = {}
 
+    # Assign values to the keys of the new country dictionary
+    new_country_dictionary["country"] = country
+    new_country_dictionary["visits"] = visits
+    new_country_dictionary["cities"] = cities
 
+    # Append the new country dictionary to the travel log
+    travel_log.append(new_country_dictionary)
 
-
-
-#🚨 Do not change the code below
+# Add a new country entry using the function
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+
+# Print the updated travel log
 print(travel_log)
