bf6878b8226a9729f52cc4d42d8dc6292f799da7
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-02 13:47:00 -0600

    feat: Completed Days in Month exercise

--- Assignments/Day_10/days_in_month/main.py
+++ Assignments/Day_10/days_in_month/main.py
@@ -19,12 +19,11 @@ def is_leap(year):
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     # Check if the year is a leap year and if the month is February (month == 2)
-    if is_leap(year) == True and month == 2:
+    if is_leap(year) and month == 2:
         # If it's a leap year and February, return 29 days
         return 29
-    else:
-        # Otherwise, return the number of days based on the month from the month_days list
-        return month_days[month - 1]
+    # Otherwise, return the number of days based on the month from the month_days list
+    return month_days[month - 1]
 
 #🚨 Do NOT change any of the code below
 year = int(input("Enter a year: "))
