5d150179f16d510088e2d4027cc704e622db4512
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-02 13:41:58 -0600

    feat: Completed Days in Month exercise

---
+++ Assignments/Day_10/days_in_month/main.py
@@ -0,0 +1,33 @@
+def is_leap(year):
+    # Check if the year is divisible by 4
+    if year % 4 == 0:
+        # If the year is divisible by 100, further check if it's divisible by 400
+        if year % 100 == 0:
+            if year % 400 == 0:
+                # If divisible by 400, it's a leap year
+                return True
+            else:
+                # If not divisible by 400, it's not a leap year
+                return False
+        else:
+            # If not divisible by 100, but divisible by 4, it's a leap year
+            return True
+    else:
+        # If not divisible by 4, it's not a leap year
+        return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # Check if the year is a leap year and if the month is February (month == 2)
+    if is_leap(year) == True and month == 2:
+        # If it's a leap year and February, return 29 days
+        return 29
+    else:
+        # Otherwise, return the number of days based on the month from the month_days list
+        return month_days[month - 1]
+
+#🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
--- Assignments/Day_10/functions/main.py
+++ Assignments/Day_10/functions/main.py
@@ -9,13 +9,10 @@
 # print(output)
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    full_name = formatted_f_name + ' ' + formatted_l_name
-    return f"{formatted_f_name} {formatted_l_name}"
+    return f"Result: {formatted_f_name} {formatted_l_name}"
 
-f_name = input("What is your first name? ")
-l_name = input("What is your last name? ")
-
-formatted_string = format_name(f_name, l_name)
-print(formatted_string)
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
