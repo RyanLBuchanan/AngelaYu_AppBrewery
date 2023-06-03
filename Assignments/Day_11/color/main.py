29745e9440f85af11e6604917eb4f5100c7aba9e
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-02 23:09:44 -0600

    feat: Completed Blackjack exercise

---
+++ Assignments/Day_11/color/main.py
@@ -0,0 +1,7 @@
+from colorama import Fore, Style
+
+# Set the text color to red
+print(Fore.RED + "This is red text" + Style.RESET_ALL)
+
+# Set the text color to green
+print(Fore.GREEN + "This is green text" + Style.RESET_ALL)
