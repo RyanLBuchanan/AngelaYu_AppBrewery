27e93e0fb3403f2b109cefcd17d0c1e020f6ec9d
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-03 11:58:44 -0600

    feat: Completed Number Guessing exercise

--- Assignments/Day_12/number_guessing_game/main.py
+++ Assignments/Day_12/number_guessing_game/main.py
@@ -1,78 +1,50 @@
 import random
+from art import logo
 from colorama import Fore, Style
+
+print(logo)
 
 num_1 = 1
 num_2 = 100
 
+# Generate a random number between num_1 and num_2
 the_number = random.randint(num_1, num_2)
 
-difficulty_level = input(f"Welcome to the Number Guessing Game\nI'm thinking of a number between {num_1} and "
-                         f"{num_2}.\nChoose a difficulty. Type 'easy' or 'hard': ").lower()
+difficulty_level = input(
+    f"Welcome to the Number Guessing Game\nI'm thinking of a number between {num_1} and {num_2}.\nChoose a difficulty. Type 'easy' or 'hard': "
+).lower()
 
+# Set the number of attempts based on the chosen difficulty level
 if difficulty_level == "easy":
     attempts = 10
 else:
     attempts = 5
 
-print(f"Psst, the number is: {the_number}.")
-
 def guess():
+    # Prompt the user to make a guess
     user_guess = int(input(f"You have {attempts} attempts remaining to guess the number.\nMake a guess: "))
 
     if user_guess == the_number:
+        # The user guessed the correct number
         return True
     elif user_guess > the_number:
+        # The user's guess is too high
         print("Too high.")
     else:
+        # The user's guess is too low
         print("Too low.")
 
+    # The user's guess was incorrect
     return False
 
+# Main game loop
 while attempts != 0:
     if guess():
+        # The user guessed the correct number
         print(f"{Fore.GREEN} \n***** You got it! The answer was {the_number}. *****\n {Style.RESET_ALL}")
         break
     attempts -= 1
 
     if attempts == 0:
+        # The user has run out of attempts
         print(f"{Fore.RED} \n***** You've run out of guesses! You lose! Wamp waaaah! *****\n {Style.RESET_ALL}")
-
-"""My original messy code below. The above was ChatGPT optimized version. Er . . .I was very close"""
-# import random
-#
-# num_1 = 1
-# num_2 = 100
-#
-# the_number = random.randint(1, 101)
-#
-# difficulty_level = input(f"Welcome to the Number Guessing Game\nI'm thinking of a number between {num_1} and "
-#                          f"{num_2}.\nChoose a difficulty. Type 'easy' or 'hard': ").lower()
-#
-# print(f"Psst, the number is: {the_number}.")
-#
-# if difficulty_level == "easy":
-#     attempts = 10
-# else:
-#     attempts = 5
-#
-# def guess(attempts):
-#     user_guess = int(input(f"You have {attempts} attempts remaining to guess the number.\nMake a guess: "))
-#
-#     if user_guess == the_number:
-#         print(f"You got it! The answer was {the_number}.")
-#         exit()
-#     elif user_guess > the_number and attempts > 0:
-#         attempts -= 1
-#         print(f"Too high.\nGuess again.")
-#     elif user_guess < the_number and attempts > 0:
-#         attempts -= 1
-#         print(f"Too low.\nGuess again.")
-#     else:
-#         attempts = 0
-#
-#     return attempts
-#
-# while attempts > 0:
-#     attempts = guess(attempts)
-#
-# print("You've run out of guesses! You lose! Wamp waaaah!")
