8838a35d856e7dba6cc4531a07f50e29dc6a41f3
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-04 17:06:38 -0600

    feat: Started Higher Lower game exercises

--- Assignments/Day_14/higher_lower/main.py
+++ Assignments/Day_14/higher_lower/main.py
@@ -11,40 +11,58 @@ def format_data(account):
 
 def check_answer(guess, a_followers, b_followers):
     """Take user guess and follower counts and returns if the user is correct"""
+    # Use if statement to check if user is correct
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
 
 # Display art
 print(logo)
 
-# Generate random accounts from game data
-account_a = random.choice(data)
+# Initialize score
+score = 0
+
+still_playing = True
+
 account_b = random.choice(data)
 
-if account_a == account_b:
+# Make the game repeatable
+while still_playing:
+    # Generate random accounts from game data
+    # Make account at position B become next account at position A
+    account_a = account_b
     account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}.")
-print(vs)
-print(f"Against B: {format_data(account_b)}.")
+    while account_a == account_b:
+        account_b = random.choice(data)
 
-# Ask user for a guess
-guess = input("Who has more followers? Tyoe 'A' or 'B': ").lower()
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
 
-# Check if user is correct
-## Get follower count of each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
+    # Ask user for a guess
+    guess = input("Who has more followers? Tyoe 'A' or 'B': ").lower()
 
-## Use if statement to check if user is correct
+    # Check if user is correct
+    # Get follower count of each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-# Give user feedback on their guess
+    # Clear the screen between rounds
+    print("\n" * 20)
+    print(logo)
+    # Give user feedback on their guess
+    # Keep score
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        still_playing = False
+        print(f"Sorry, that's wrong. Final score: {score}")
 
-# Keep score
 
-# Make the game repeatable
-
-# Make account at position B become next account at position A
-
-# Clear the screen between rounds
 
 
 
