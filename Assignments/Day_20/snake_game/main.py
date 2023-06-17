caa80c8a55d9983d7ad66f50f60e930e33988ed3
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-17 10:12:37 -0600

    feat: Completed first challenge to set up three sections of snake

--- Assignments/Day_20/snake_game/main.py
+++ Assignments/Day_20/snake_game/main.py
@@ -1,3 +1,32 @@
+import turtle
 from turtle import Screen, Turtle
 
 screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+# Create the turtle or snake instances
+snake_sections = []
+x_positions = [n * -20 for n in range(3)]
+
+for snake_index in range(0, 3):
+    new_snake = Turtle(shape="square")
+    new_snake.penup()
+    new_snake.color("white")
+    new_snake.goto(x=x_positions[snake_index], y=0)
+    snake_sections.append(new_snake)
+    # snake.shapesize(stretch_wid=1.0, stretch_len=3.0)
+
+
+# Function for exiting the program with the space bar
+def exit_program():
+    turtle.bye()
+
+
+# Bind the exit program function to the space bar
+screen.onkeypress(exit_program, "space")
+
+screen.listen()
+
+screen.exitonclick()
