6f2bde51f7ff73bf6d50a8e5080bce2113a5f799
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-17 11:53:32 -0600

    feat: Refactored movement of snake into Snake class and methods

--- Assignments/Day_20/snake_game/main.py
+++ Assignments/Day_20/snake_game/main.py
@@ -2,19 +2,26 @@ from turtle import Screen, Turtle
 from snake import Snake
 import time
 
+# Create the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
+# Create the snake
 snake = Snake()
 
 game_is_on = True
 while game_is_on:
+    # Update the screen
     screen.update()
+
+    # Delay the movement
     time.sleep(0.1)
 
+    # Move the snake
     snake.move()
 
+# Close the screen on click
 screen.exitonclick()
--- Assignments/Day_20/snake_game/snake.py
+++ Assignments/Day_20/snake_game/snake.py
@@ -1,17 +1,24 @@
-from turtle import Screen, Turtle
+from turtle import Turtle
 
-# Create the turtle or snake instances
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-"""" List comprehension for the above list: [n * -20 for n in range(3)] 
--> new_segment.goto(x=starting_positions[position], y=0)"""
 
 class Snake:
     def __init__(self):
+        """
+        Initialize the Snake class.
+        """
         self.segments = []
         self.create_snake()
 
     def create_snake(self):
+        """
+        Create the initial snake by creating turtle segments and positioning them.
+        """
+        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        # The starting positions for each segment of the snake. Adjust the values as needed.
+        # List comprehension for the above list: [n * -20 for n in range(3)]
+        # -> new_segment.goto(x=starting_positions[position], y=0)
         for position in starting_positions:
+            # Iterate over the starting positions list
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
@@ -19,6 +26,9 @@ class Snake:
             self.segments.append(new_segment)
 
     def move(self):
+        """
+        Move the snake by updating the positions of each segment.
+        """
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
