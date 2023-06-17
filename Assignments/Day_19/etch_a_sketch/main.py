b05dc69fde5d30e54b99113e18a802acc35db5a7
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-16 20:57:04 -0600

    feat: Almost there . . .

--- Assignments/Day_19/etch_a_sketch/main.py
+++ Assignments/Day_19/etch_a_sketch/main.py
@@ -2,35 +2,33 @@ import turtle
 import random
 from turtle import Turtle, Screen
 
-from zmq import green
-
-
-# Function to generate random color values
-def get_random_color():
-    r = random.random()
-    g = random.random()
-    b = random.random()
-    return r, g, b
-
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_positions = [-70, -40, -10, 20, 50, 80]
+user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color: ")
+y_positions = [-100, -50, 0, 50, 100, 150]
 
 turtles = []
 
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    turtles.append(new_turtle)
 
-for turtle_index in range(0, 5):
-    turtle = Turtle()
-    turtle.shape("turtle")
-    turtle.color(colors[turtle_index])
-    turtles.append(turtle)
-    turtle.penup()
-    turtle.goto(x=-230, y=y_positions[turtle_index])
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            print(turtle.color())
+            is_race_on = False
 
-screen = Screen()
-screen.setup(width=500, height=400)
-
-user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color: ")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 
 # Function for exiting turtle with space bar
@@ -45,6 +43,14 @@ random_speeds = []
 for _ in range(6):
     random_move_param = random.randint(10, 50)
     random_speeds.append(random_move_param)
+
+
+# Function to generate random color values
+def get_random_color():
+    r = random.random()
+    g = random.random()
+    b = random.random()
+    return r, g, b
 
 
 def move_forwards():
@@ -64,7 +70,7 @@ def turn_left():
 
 def turn_right():
     for turtle in turtles:
-        turn_right(random_move_param)
+        turtle.right(random_move_param)
 
 
 def clear():
@@ -76,13 +82,11 @@ def clear():
 
 screen.listen()
 
-
-#Function as input
+# Function as input
 screen.onkey(key="w", fun=move_forwards)
 screen.onkey(key="s", fun=move_backwards)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
 screen.onkey(key="c", fun=clear)
-
 
 screen.exitonclick()
