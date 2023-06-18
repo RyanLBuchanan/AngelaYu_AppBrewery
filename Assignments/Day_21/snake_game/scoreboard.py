aeefed9efb56d07a2d65dea53b45f1ba04f27fad
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-18 13:55:26 -0600

    feat: Refactored scoreboard code for optimization

--- Assignments/Day_21/snake_game/scoreboard.py
+++ Assignments/Day_21/snake_game/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Press Start 2P", 16, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -9,19 +11,18 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.speed("fastest")
         self.goto(0, 470)
-        self.font = ("Press Start 2P", 16, "normal")
-        self.write_score()
+        self.update_scoreboard()
 
-    def write_score(self):
+    def update_scoreboard(self):
         """
         Write the current score on the screen using the custom font.
         """
         self.clear()  # Clear any previously written score
-        self.write(f"Score: {self.score}", align="center", font=self.font)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         """
         Increase the score by 1 and update the displayed score.
         """
         self.score += 1
-        self.write_score()
+        self.update_scoreboard()
