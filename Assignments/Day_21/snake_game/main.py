bfc6f0f0feb8b4266b949a859bf8d7fcd34305cf
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-17 20:40:11 -0600

    feat: Started Day 21

---
+++ Assignments/Day_21/snake_game/main.py
@@ -0,0 +1,38 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+# Create the screen
+screen = Screen()
+screen.setup(width=1000, height=1000)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+# Create the snake
+snake = Snake()
+
+# Events listeners
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+# Bind the exit program function to the space bar
+screen.onkeypress(snake.exit_program, "space")
+
+game_is_on = True
+while game_is_on:
+    # Update the screen
+    screen.update()
+
+    # Delay the movement
+    time.sleep(0.25)
+
+    # Move the snake
+    snake.move()
+
+# Close the screen on click
+screen.exitonclick()
---
+++ Assignments/Day_21/snake_game/snake.py
@@ -0,0 +1,83 @@
+from turtle import Turtle
+
+# Create constant references
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        """
+        Initialize the Snake class.
+        """
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        """
+        Create the initial snake by creating turtle segments and positioning them.
+        """
+        for position in STARTING_POSITIONS:
+            # Iterate over the starting positions list
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        """
+        Move the snake by updating the positions of each segment.
+        """
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """
+        Turn the snake to move up
+        """
+        # If not heading down, the snake can move up
+        if self.head.heading() != DOWN:
+            # Set the current heading of the snake's head
+            self.head.setheading(UP)
+
+    def down(self):
+        """
+        Turn the snake to move down
+        """
+        # If not heading up, the snake can move down
+        if self.head.heading() != UP:
+            # Set the current heading of the snake's head
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """
+        Turn the snake to move left
+        """
+        # If not heading right, the snake can move left
+        if self.head.heading() != RIGHT:
+            # Set the current heading of the snake's head
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """
+        Turn the snake to move right
+        """
+        # If not heading left, the snake can move right
+        if self.head.heading() != LEFT:
+            # Set the current heading of the snake's head
+            self.head.setheading(RIGHT)
+
+    def exit_program(self):
+        """
+        Exit the program with the space bar
+        """
+        self.head.screen.bye()
+
