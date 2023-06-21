25269d4fdd1b83ee7a79f2c7d84edb15715b08ca
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-21 08:13:57 -0600

    feat: Added read and write methods to save new high scores to data.txt

--- Assignments/Day_24/snake_game/scoreboard.py
+++ Assignments/Day_24/snake_game/scoreboard.py
@@ -1,13 +1,16 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Press Start 2P", 16, "normal")
 SCOREBOARD_COORDINATES = 0, 475
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("orange")
         self.penup()
         self.hideturtle()
@@ -32,5 +35,16 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
+
+    # def get_high_score(self):
+    #     with open("data.txt", mode="r") as file:
+    #         self.high_score = int(file.read())
+    #     return self.high_score
+    #
+    # def set_high_score(self):
+    #     with open("data.txt", mode="w") as file:
+    #         file.write(str(self.high_score))
