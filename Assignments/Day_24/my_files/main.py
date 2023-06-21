ff37cb258d5eac6b9d73ea63dc8f679f1db88ee9
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-21 09:23:34 -0600

    feat: Added starting code and directions for Mail Merge exercise

--- Assignments/Day_24/my_files/main.py
+++ Assignments/Day_24/my_files/main.py
@@ -1,9 +1,18 @@
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
 
 # with open("my_file.txt", mode="a") as file:
 #     file.write("\nNew stuff!")
 
-with open("new_file.txt", mode="w") as file:
-    file.write("\nNew stuff!")
+with open("../../", mode="w") as file:
+    file.write("\nSpecial stuff!")
+
+# import os
+#
+# file_path = 'C:/Users/vreed/Desktop/Python/Udemy/AngelaYu/AngelaYu_AppBrewery/Assignments/Day_24/my_files/my_file.txt'
+#
+# if os.access(file_path, os.W_OK):
+#     print(file_path)
+# else:
+#     print("no worky stuff")
--- Assignments/Day_24/snake_game/scoreboard.py
+++ Assignments/Day_24/snake_game/scoreboard.py
@@ -35,7 +35,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open("data.txt", mode="w") as data:
+            with open("C:/Users/vreed/Desktop/Python/Udemy/AngelaYu/AngelaYu_AppBrewery/Assignments/Day_24/my_files/data.txt", mode="w") as data:
                 data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
