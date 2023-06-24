1fa490a95254fb34911826bf121b3f8c787b6e96
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-24 06:28:11 -0600

    feat: Completed tkinter initial setup and explore

--- Assignments/Day_27/tkinter/main.py
+++ Assignments/Day_27/tkinter/main.py
@@ -1,17 +1,35 @@
-import tkinter as tk
-FONT = ("Press Start 2P", 24, "normal")
+from tkinter import *
+
+FONT = ("Press Start 2P", 16, "normal")
+
+# Function for closing on space bar
 def exit_program(event):
     window.destroy()  # Destroy the Tk window
 
 
-window = tk.Tk()
+window = Tk()
 window.title("Hello GUI")
 window.minsize(width=500, height=300)
 
 # Label
+my_label = Label(text="I am a label.", font=FONT, fg="orange")
+my_label.pack()  # Alignment with "side="left"
+my_label["text"] = "New text"
 
-my_label = tk.Label(text="I am a label.", font=FONT, fg="orange")
-my_label.pack(side="left")
+# Button click function
+def button_clicked():
+    my_label["text"] = "I got clicked!"
+
+
+# Button
+my_button = Button(text="Click me!", font=FONT, command=button_clicked, fg="orange")  # Color button fg="blue", bg="green"
+my_button.pack()
+
+
+
+
+
+# my_label.config(text="Newer text", fg="green", bg="gray")
 
 # Bind the space bar key to the exit_program function
 window.bind("<space>", exit_program)
