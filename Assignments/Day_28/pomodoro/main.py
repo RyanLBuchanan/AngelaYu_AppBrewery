aa9c251916622b38107aef52031f6bd8d7598458
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-24 20:47:55 -0600

    feat: Created functions for count down and start timer

--- Assignments/Day_28/pomodoro/main.py
+++ Assignments/Day_28/pomodoro/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -14,39 +15,75 @@ FONT_PRESS_START = "Press Start 2P"
 BGCOLOR_DARKTHEME1 = "#1F3B4D"
 BGCOLOR_DARKTHEME2 = "#92A8A0"
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+
+def reset_timer():
+    pass
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    # Call count down function
+    count_down(WORK_MIN * 60)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec:02d}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Create the main window
 window = Tk()
+
+# Set the title of the window
 window.title("Pomodoro")
+
+# Configure the window's padding and background color
 window.config(padx=100, pady=50, bg=BGCOLOR_DARKTHEME2)
 
+# Create a canvas for displaying the timer image and text
 canvas = Canvas(width=200, height=224, bg=BGCOLOR_DARKTHEME2, highlightthickness=0)
+
+# Load and display the tomato image on the canvas
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_PRESS_START, 12, "normal"))
+
+# Display the initial timer text on the canvas
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_PRESS_START, 20, "normal"))
+
+# Position the canvas in the window's grid
 canvas.grid(column=1, row=1)
 
-timer_label = Label(text="Timer", fg="yellow", bg=BGCOLOR_DARKTHEME2, font=(FONT_PRESS_START, 20, "normal"), padx=20, pady=20)
+# Create a label for the timer
+timer_label = Label(text="Timer", fg="green", bg=BGCOLOR_DARKTHEME2, font=(FONT_PRESS_START, 50, "normal"), padx=20, pady=20)
 
+# Position the timer label in the window's grid
 timer_label.grid(column=1, row=0)
 
-checkmark_label = Label(text="✔", fg="yellow", bg=BGCOLOR_DARKTHEME2, font=(FONT_PRESS_START, 20, "normal"), padx=20, pady=20)
+# Create a label for displaying checkmarks
+checkmarks_label = Label(text="✔", fg="green", bg=BGCOLOR_DARKTHEME2, font=(FONT_PRESS_START, 20, "normal"), padx=20, pady=20)
 
-checkmark_label.grid(column=1, row=4)
+# Position the checkmarks label in the window's grid
+checkmarks_label.grid(column=1, row=4)
 
-start_button = Button(text="Start", font=(FONT_PRESS_START, 8, "normal"))
+# Create a button for starting the timer
+start_button = Button(text="Start", fg="orange", font=(FONT_PRESS_START, 8, "normal"), bg=BGCOLOR_DARKTHEME1, padx=5, pady=5, highlightthickness=0, command=start_timer)
 
+# Position the start button in the window's grid
 start_button.grid(column=0, row=3)
 
-reset_button = Button(text="Reset", font=(FONT_PRESS_START, 8, "normal"))
+# Create a button for resetting the timer
+reset_button = Button(text="Reset", fg="orange", font=(FONT_PRESS_START, 8, "normal"), bg=BGCOLOR_DARKTHEME1, padx=5, pady=5, highlightthickness=0)
 
+# Position the reset button in the window's grid
 reset_button.grid(column=2, row=3)
+
 
 
 
