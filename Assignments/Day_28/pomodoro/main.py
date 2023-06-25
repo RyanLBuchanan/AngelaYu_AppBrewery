f9ce4e98e7d6ca2104f1f2fed3111b23b22cc3ac
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-24 20:14:41 -0600

    feat: Setup initial UI for Pomodoro app

--- Assignments/Day_28/pomodoro/main.py
+++ Assignments/Day_28/pomodoro/main.py
@@ -10,7 +10,7 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 # Constants
-FONT = ("Press Start 2P", 12, "normal")
+FONT_PRESS_START = "Press Start 2P"
 BGCOLOR_DARKTHEME1 = "#1F3B4D"
 BGCOLOR_DARKTHEME2 = "#92A8A0"
 
@@ -29,8 +29,25 @@ window.config(padx=100, pady=50, bg=BGCOLOR_DARKTHEME2)
 canvas = Canvas(width=200, height=224, bg=BGCOLOR_DARKTHEME2, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=FONT)
-canvas.pack()
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_PRESS_START, 12, "normal"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", fg="yellow", bg=BGCOLOR_DARKTHEME2, font=(FONT_PRESS_START, 20, "normal"), padx=20, pady=20)
+
+timer_label.grid(column=1, row=0)
+
+checkmark_label = Label(text="✔", fg="yellow", bg=BGCOLOR_DARKTHEME2, font=(FONT_PRESS_START, 20, "normal"), padx=20, pady=20)
+
+checkmark_label.grid(column=1, row=4)
+
+start_button = Button(text="Start", font=(FONT_PRESS_START, 8, "normal"))
+
+start_button.grid(column=0, row=3)
+
+reset_button = Button(text="Reset", font=(FONT_PRESS_START, 8, "normal"))
+
+reset_button.grid(column=2, row=3)
+
 
 
 
