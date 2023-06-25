7c1c3fe8358f979b0f81ff1cfed44e32550c96f6
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-24 18:19:55 -0600

    feat: Setup initial UI for Pomodoro app

---
+++ Assignments/Day_28/pomodoro/main.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+canvas.pack()
+
+
+
+# ---------------------------- Development Tools ------------------------------- #
+# Function for closing on space bar
+def exit_program(event):
+    window.destroy()  # Destroy the Tk window
+
+
+# Bind the space bar key to the exit_program function
+window.bind("<space>", exit_program)
+
+
+# Start the main event loop to display the window and handle user interactions
+window.mainloop()
