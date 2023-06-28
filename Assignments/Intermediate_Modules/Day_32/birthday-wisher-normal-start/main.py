3d402428d5aed26de0a09714b9033948352ad7e6
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-28 17:56:48 -0600

    feat: Created gitignore for .env variables of username and password

--- Assignments/Intermediate_Modules/Day_32/birthday-wisher-normal-start/main.py
+++ Assignments/Intermediate_Modules/Day_32/birthday-wisher-normal-start/main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import pandas as pd
 import smtplib
 import random
+import time
 import os
 from dotenv import load_dotenv
 
@@ -10,27 +11,29 @@ load_dotenv() # Load environment variables from .env file
 MY_EMAIL = os.getenv("MY_EMAIL")
 MY_PASSWORD = os.getenv("MY_PASSWORD")
 
-TODAY = datetime.now()
-TODAY_TUPLE = (TODAY.month, TODAY.day)
+# Loop indefinitely
+while True:
+    TODAY = datetime.now()
+    TODAY_TUPLE = (TODAY.month, TODAY.day)
 
+    data = pd.read_csv("birthdays.csv")
+    birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-data = pd.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+    if TODAY_TUPLE in birthdays_dict:
+        birthday_person = birthdays_dict[TODAY_TUPLE]
+        file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+        with open(file_path) as letter_file:
+            contents = letter_file.read()
+            contents = contents.replace("[NAME]", birthday_person["name"])
 
-if TODAY_TUPLE in birthdays_dict:
-    birthday_person = birthdays_dict[TODAY_TUPLE]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-    
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs="buchanan.ryan22@gmail.com",
-            msg=f"Subject:Happy Birthday, TURD!\n\n{contents}"
-        )
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="buchanan.ryan22@gmail.com",
+                msg=f"Subject:Happy Birthday, TURD!\n\n{contents}"
+            )
 
-
+    # Delay for 24 hours before executing the code again
+    time.sleep(24 * 60 * 60)
--- Assignments/Intermediate_Modules/Day_32/prep_for_project/main.py
+++ Assignments/Intermediate_Modules/Day_32/prep_for_project/main.py
@@ -2,40 +2,38 @@ import pandas as pd
 import smtplib
 import datetime as dt
 import random
+import time
+import os
+from dotenv import load_dotenv
 
-MY_EMAIL = "ryan.py.test@gmail.com"
-MY_PASSWORD = "cobcghtixdcgimey"
+load_dotenv() # Load environment variables from .env file
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
+
 INPUT_FILE = "quotes.txt"
 
-now = dt.datetime.now()
-day_of_week = now.weekday()
+# Loop indefinitely
+while True:
+    now = dt.datetime.now()
+    day_of_week = now.weekday()
 
+    if day_of_week == 2:
+        # Read quotes from the text file
+        with open(INPUT_FILE, "r") as file:
+            all_quotes = file.readlines()
+            quote = random.choice(all_quotes)
 
-if day_of_week == 2:
-    # Read quotes in from the text file
-    with open(INPUT_FILE, "r") as file:
-        all_quotes = file.readlines()
-        quote = random.choice(all_quotes)
+        print(quote)
 
-    print(quote)
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="buchanan.ryan22@gmail.com",
+                msg=f"Subject:Morning Inspiration\n\n{quote}"
+            )
 
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs="buchanan.ryan22@gmail.com",
-            msg=f"Subject:Morning Inspiration\n\n{quote}"
-        )
-#
-#
-#
-# # year = now.year
-# #
-# # day_of_week = now.weekday()
-# #
-# # if day_of_week == 2:
-#
-# # date_of_birth = dt.datetime(year=1974, month=6, day=4, hour=3)
-#
-# # connection.close()
+    # Delay for 24 hours before executing the code again
+    time.sleep(24 * 60 * 60)
