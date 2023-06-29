85a95208084f521dec714f92401962ca2162f963
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-29 15:59:51 -0600

    feat: Completed API call for Quizzler app questions and started UI setup with Tkinter

--- Assignments/Intermediate_Modules/Day_34/quizzler_app/ui.py
+++ Assignments/Intermediate_Modules/Day_34/quizzler_app/ui.py
@@ -1,2 +1,22 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+
+
+        self.window.mainloop()
+
+
+        # Function for closing on space bar
+        def exit_program(event):
+            self.window.destroy()  # Destroy the Tk window
+
+        # Bind the space bar key to the exit_program function
+        self.window.bind("<space>", exit_program)
+
+        self.window.mainloop()
