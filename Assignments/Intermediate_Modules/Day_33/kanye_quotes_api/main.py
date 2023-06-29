164af04400962a189f86a21f1fe1fd1f0d8ebca0
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-28 21:25:05 -0600

    feat: Completed Kanye API challenge

---
+++ Assignments/Intermediate_Modules/Day_33/kanye_quotes_api/main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import requests
+
+
+def get_quote():
+    response = requests.get("https://api.kanye.rest")
+    data = response.json()
+    print(data['quote'])
+    canvas.itemconfig(quote_text, text=data['quote'])
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+# ---------------------------- Development Tools ------------------------------- #
+# Function for closing on space bar
+def exit_program(event):
+    window.destroy()  # Destroy the Tk window
+
+
+# Bind the space bar key to the exit_program function
+window.bind("<space>", exit_program)
+
+window.mainloop()
