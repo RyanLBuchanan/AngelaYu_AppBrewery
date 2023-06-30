ca27eaae033bc6a2ee9bdbe8ee45471e6b0098f6
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-30 15:11:19 -0600

    feat: Set up initial API request to OpenWeather with personal API key

---
+++ Assignments/Intermediate_Modules/Day_35/open_weather_api/main.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from .env file
+
+MY_LAT = 41.222759
+MY_LONG = -111.970421
+
+MY_API_KEY = os.getenv("API_KEY")
+
+PARAMETERS = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": MY_API_KEY,
+}
+
+"""
+Complete API with parameters and my API key:
+https://api.openweathermap.org/data/2.5/weather?lat=41.222759&lon=-111.970421&appid=MY_API_KEY
+"""
+
+response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=PARAMETERS)
+response.raise_for_status()
+data = response.json()
+print(data)
