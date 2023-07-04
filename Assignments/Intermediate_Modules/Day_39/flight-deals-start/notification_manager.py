eb12baecb60a1a6246bec273b35b79e57fdf77fe
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-07-04 15:08:13 -0600

    feat: Completed Flight notification app through Step 5 - still need to debug

--- Assignments/Intermediate_Modules/Day_39/flight-deals-start/notification_manager.py
+++ Assignments/Intermediate_Modules/Day_39/flight-deals-start/notification_manager.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
-import requests
 from twilio.rest import Client
-from datetime import datetime
 
 load_dotenv()
 
@@ -14,29 +12,14 @@ class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
 
     def __init__(self):
-
-        self.prices = []
-
-        self.price = price
-        self.city = city
-        self.aita = aita
-
-        self.today = datetime.now()
-        self.return_date = datetime.now() + datetime.timedelta()
+        self.client = Client(account_sid, auth_token)
 
 
-        # Create a new list of the city-airport and prices using list comprehension.
-        self.formatted_price_updates = [f"Low price alert! Only ${self.price} to fly from {self.city}-{self.aita} from {self.today.strftime('%Y-%m-%d')} to {self.return_date.strftime('%Y-%m-%d')}." for price in prices]
-
-
-    def send_notification(self):
+    def send_sms(self, message):
         # Send each article as a separate message via Twilio.
-        client = Client(account_sid, auth_token)
-        for price in self.formatted_price_updates:
-            message = client.messages \
-                .create(
-                body=price,
-                from_="+18554760916",
-                to="+13854329281"
+        message = self.client.messages.create(
+            body=message,
+            from_="+18554760916",
+            to="+13854329281"
             )
-            print(message.status)
+        print(message.sid)
