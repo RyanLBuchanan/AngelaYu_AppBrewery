0d812063c711784cf7795c055afb4f9b5b00c76e
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-07-10 06:48:34 -0600

    feat: Completed Beautiful Soup Scarping Live Websites exercises

--- Assignments/Intermediate_Modules/Day_45/main.py
+++ Assignments/Intermediate_Modules/Day_45/main.py
@@ -1,10 +1,41 @@
 from bs4 import BeautifulSoup
 import lxml # If HTML parser does not support
+import requests
+
+response = requests.get("https://news.ycombinator.com/news")
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+# soup_articles = soup.select(".titleline a")
+#
+# for article in soup_articles:
+#     print(article.text)
+
+articles = soup.find_all(name="a", rel="noreferrer")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+# print(max(article_upvotes))
+
+max_upvote = max(article_upvotes)
+max_index = article_upvotes.index(max_upvote)
 
 
-
-
-
+print(article_texts[max_index])
+print(article_links[max_index])
+print(article_upvotes[max_index])
 
 
 
