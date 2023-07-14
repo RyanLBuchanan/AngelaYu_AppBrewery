ebb8db9cd071a93edb6a5d7fd38562b8811ce76e
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-07-14 10:23:08 -0600

    feat: Installed Chrome driver

--- Assignments/Intermediate_Modules/Day_48/main.py
+++ Assignments/Intermediate_Modules/Day_48/main.py
@@ -1,1 +1,11 @@
-""" Selenium Web Scraping"""
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+chrome_driver_path = "C:/Users/vreed/Documents/Development/chromedriver"
+service = Service(executable_path=chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+
+
+driver.get("https://www.amazon.com")
+# driver.close()
+driver.quit()
